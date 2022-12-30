29bb20f590e94c32dbe597c88ffa19d7897de6c9
Thatagricguy <samueljohn3999@gmail.com>
2022-12-29 22:48:17 -0800

    working with srings

---
+++ working_strings/replace_string.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+
+int main() 
+{
+  char s[] = "When the zombies arrive, quickly fax Judge Pat Alphabet";
+  char p[] = "poolloop";  
+  for(int i = 0; i < strlen(s); i++)
+  {
+  char s[] = "*";
+  printf("%s\n", s);
+  }
+for(int j = 0; j < strlen(p)/2; j++)
+{
+  p[j] = '#';
+  
+  printf("%s\n", p);
+}
+return 0;
+}
