7310e0cf1e13c0de86e9690be0a6240a70efbb31
Thatagricguy <samueljohn3999@gmail.com>
2022-12-31 15:26:44 -0800

    working with structure in c

---
+++ structures/initialize_struct.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+/**
+  * How to initialize structures using ordered method
+  */
+
+int main(void) 
+{
+  struct Person 
+  {
+    char firstName[25];
+    char lastName[40];
+    
+    int age;
+  };
+  
+  struct Person person1 =
+  {"Ada", "Lovelace", 28};
+ struct Person person2 =
+ {"Marie", "Curie", 44}; 
+}
