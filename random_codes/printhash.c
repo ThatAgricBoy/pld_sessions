78824baacf3a350432c6316dd02a94adbf930f8b
Thatagricguy <samueljohn3999@gmail.com>
2023-01-10 21:25:15 -0800

    print hash

---
+++ random_codes/printhash.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+int main (void)
+{
+	int height = 6;
+	int width = 5;
+	for (height = 0; height <= 6; height++)
+	{
+	
+		for(width = height; width <=5; width++)
+		{ 
+			printf("# #\n");
+		}
+	}
+
+
+	
+	return (0);
+
+}
