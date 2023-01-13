b1aa8b1d1abcbcb0ee1d166190d171c34b73eb6b
Thatagricguy <samueljohn3999@gmail.com>
2023-01-13 04:01:29 -0800

    memcpy

---
+++ functions1/memcpy.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <srtring.h>
+
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+	i =0;
+
+	while (i < n)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	return (dest);
+}
