ac62eb606003067ec0237e31628b72811692ed59
Thatagricguy <samueljohn3999@gmail.com>
2023-01-22 14:20:11 -0800

    ex11

---
+++ hard_c/ex11.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(int agrc, char *argv[])
+{
+	//go through each string in argv
+
+	int i = 0;
+	while(i < argc)
+	{
+		printf("arg %d: %s\n", i, argv[]
+				i++;
+	}
+
+	//lets make out our own array of strings
+
+	char *states[] = {"California", "Oregon", "Washington", "Texas"};
+
+	int num_states = 4;
+	i = 0; //watch for this
+	while(i < num_states)
+	{
+		printf("stae %d: %s\n", states[i]
+
+	}
+	return 0;`
+}
