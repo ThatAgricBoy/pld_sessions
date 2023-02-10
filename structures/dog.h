b655519675a7766b42fc0fefbe91f69f6279af60
Thatagricguy <samueljohn3999@gmail.com>
2023-02-10 21:24:30 +0000

    struct and functions

--- structures/dog.h
+++ structures/dog.h
@@ -1,15 +1,13 @@
 #include <stdio.h>
 #ifndef DOG_H
 #define DOG_H
-int main()
-{
-  struct dog
-  {
-    char *name;
-    char *owmer;
-    float age;
-  };
-}
+
+ struct dog
+ {
+ char *name;
+ char *owner;
+ float age;
+ };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
--- structures/init.c
+++ structures/init.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
-#include 
+#include "dog.h"
 /**
  * initializing struct from a header file
  */
-
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if(d != NULL)
+	{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+	}
+}
