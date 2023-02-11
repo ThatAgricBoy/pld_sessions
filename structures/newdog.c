f3d0d90aa506f6b662d84b8d6f729f00f40169b7
max-techs <samueljohnmaxi@gmail.com>
2023-02-11 15:57:08 +0100

    adjusted the newdog file to use string library

--- structures/newdog.c
+++ structures/newdog.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include "dog.h"
-int _strlen(char *s);
-char *_strcpy(char *dest, char *src);
+#include <string.h>
 /**
   * A function that creates a new structure and then copies and stores 
   * it in another new struct.
@@ -16,19 +15,19 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *doggo = malloc(sizeof(dog_t));
 	if(doggo == NULL)
 		return (NULL);
-	_strlen(name);
-	lename = _strlen(name);
+	strlen(name);
+	lename = strlen(name);
 	nename = malloc((lename + 1) * sizeof(char));
 	if(nename == NULL)
 	{
 		free(doggo);
 		return(NULL);
 	}
-	_strcpy(nename, name);
+	strcpy(nename, name);
 	
-	_strlen(owner);
+	strlen(owner);
 
-	leowner = _strlen(owner);
+	leowner = strlen(owner);
 	neowner = malloc((leowner +1) * sizeof(char));
 	if(neowner == NULL)
 	{
@@ -36,37 +35,11 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(doggo);
 		return (NULL);
 	}
-	_strcpy(neowner, owner);
+	strcpy(neowner, owner);
 	
 	doggo->name = nename;
 	doggo->age = age;
 	doggo->owner = neowner;
 	
 	return (doggo);
-}
-
-/**
-  *The _strcpy and _strlen definitions
-  */
-int _strlen(char *s)
-{
-	int i = 0;
-	while(*s != '\0')
-	{
-		i++;
-		s++;
-	}
-
-	return(i);
-}
-
-char *_strcpy(char *dest, char *src)
-{
-	int j;
-	for(j = 0; src[j]; j++)
-	{
-		dest[j] = src[j];
-	}
-	dest[j] = '\0';
-	return dest;
 }
