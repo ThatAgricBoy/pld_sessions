1e78020727ef51c4b76555ecd768f414b60c3011
Thatagricguy <samueljohn3999@gmail.com>
2023-02-25 13:47:49 -0800

    sum them all

--- variadic_func/0-sum_them_all.c
+++ variadic_func/0-sum_them_all.c
@@ -3,15 +3,36 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
+
 	va_list all;
 
 	int i;
 	int sum = 0;
 
-	va_start(all n);
+	va_start(all, n);
 
 	for (i = 0; i < n; i++)
-	{
-		sum += va_arg(all 
+	{	if (n == 0)
+		{
+			return (0);
+		}
+		else
+		{
+		sum += va_arg(all, int);
+		}
 	}
+	va_end(all);
+
+	return sum;
+}
+
+int main(void)
+{
+    int sum;
+
+    sum = sum_them_all(2, 98, 1024);
+    printf("%d\n", sum);
+    sum = sum_them_all(4, 98, 1024, 402, -1024);
+    printf("%d\n", sum);    
+    return (0);
 }
