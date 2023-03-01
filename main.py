263f684ebeac0caca8677b3a1d68f2021c368d33
Samuel John <117738855+ThatAgricBoy@users.noreply.github.com>
2023-03-01 11:58:29 +0000

    concatenating strings and using newlines

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ print("Day 1 - Python Print Function")
 
 print("The function is declared like this:")
 
-print("print('what to print')")
+print("print('what to print')\n")
 
 
-
+print("Using the new line\n to try and print on two line\n or more\n")
+# concatenating strings
+print("Hello" " "+ "Samuel")
 
 
 
